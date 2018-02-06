831db18d7d94fd5d1aad52889b3a2c0b18fadea3
Anna Kotnowska <anna.kotlowska@gmail.com>
2018-02-06 10:56:42 +0100

    eslint

--- test.js
+++ test.js
@@ -1,6 +1,20 @@
 import test from 'ava';
+import linearRegression from './';
 
-test('Offset a number by an amount', t => {
+test('should pass', t => {
+    const data = [
+        [43, 99],
+        [21, 65],
+        [25, 79],
+        [42, 75],
+        [57, 87],
+        [59, 81]
+    ];
 
-    t.is('lala', 'lala');
+    const expected = linearRegression(data);
+
+    t.is(expected, {
+        a: 65.1415715245131,
+        b: 0.38522498321020804
+    });
 });
